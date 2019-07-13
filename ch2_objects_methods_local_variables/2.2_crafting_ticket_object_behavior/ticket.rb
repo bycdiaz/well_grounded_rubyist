ac325b8b4d051100d3bbd2f0a80ca0c8b2fc9ac6
cdiaz <cdiaz86@gmail.com>
2019-07-13 18:00:34 -0400

    Adds notes for afternoon study session.

--- ch2_objects_methods_local_variables/2.2_crafting_ticket_object_behavior/ticket.rb
+++ ch2_objects_methods_local_variables/2.2_crafting_ticket_object_behavior/ticket.rb
@@ -24,13 +24,18 @@ def ticket.price
   5.50
 end
 
-print "This ticket is for: "
-print ticket.event + ", at "
-print ticket.venue + ", on "
-puts ticket.date + "."
-print "The Performer is "
-puts ticket.performer + "."
-print "The seat is "
-print ticket.seat + ", "
-print "and it costs $"
-puts "%.2f." % ticket.price
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" +
+"The performer is #{ticket.performer}.\n" +
+"The seat us #{ticket.seat}, " +
+"and it costs $#{"%.2f" % ticket.price}"
+
+# print "This ticket is for: "
+# print ticket.event + ", at "
+# print ticket.venue + ", on "
+# puts ticket.date + "."
+# print "The Performer is "
+# puts ticket.performer + "."
+# print "The seat is "
+# print ticket.seat + ", "
+# print "and it costs $"
+# puts "%.2f." % ticket.price
