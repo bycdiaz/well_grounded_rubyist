b2aef9d3b90625c897dee7a2c40d352a6f6d42c2
bycdiaz <cdiaz86@gmail.com>
2019-08-18 20:19:13 -0400

    Adds notes for 08/18/2019.

--- ch6_control-flow_techniques/6.3.6_importance_of_being_each.rb
+++ ch6_control-flow_techniques/6.3.6_importance_of_being_each.rb
@@ -1,1 +1,20 @@
-left off on page 182
+# example of each operation
+
+array = [1,2,3,4,5]
+# array.each { |e| puts "The block just got handed #{e}."}
+# returns array, since each only yields to each value of the array
+
+# creating my_each method
+
+class Array
+  def my_each
+    c = 0
+    until c == size
+      yield self[c]
+      c += 1
+    end
+    self
+  end
+end
+
+array.my_each {|e| puts "The block just got handed #{e}." }
