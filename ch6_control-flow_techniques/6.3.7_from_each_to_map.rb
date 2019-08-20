80523c034e88ef65107c4b9657cc8c9c069c2764
bycdiaz <cdiaz86@gmail.com>
2019-08-19 21:05:21 -0400

    Adds notes for 08/19/2019.

--- ch6_control-flow_techniques/6.3.7_from_each_to_map.rb
+++ ch6_control-flow_techniques/6.3.7_from_each_to_map.rb
@@ -1,1 +1,51 @@
-left off pn page 184
+# each returns receiver of method, map returns a new array.
+
+# array = [1,2,3,4]
+
+# puts "This is the return value of each:"
+# p array.each { |num| num + 1}
+
+# puts "This is the return value of map:"
+# p array.map { |num| num * num}
+
+# another example:
+
+names = ["David","Alan", "Black"]
+
+# p names.map { |name| name.upcase}
+
+
+# implimentation of map
+
+class Array
+  def my_map
+    c = 0
+    acc = []
+    until c == size
+      acc << yield self[c]
+      c += 1
+    end
+    acc
+  end
+end
+names.my_map {|name| name.upcase }
+
+
+# using each as a base for map
+
+class Array
+  def my_each
+    c = 0
+    until c == size
+      yield self[c]
+      c += 1
+    end
+    self
+  end
+
+  def my_map
+    acc = []
+    my_each {|e| acc << yield e }
+    acc
+  end
+end
